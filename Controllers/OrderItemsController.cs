6c8aeac6d46acd24615011e9adf77f266b44eeff
DanielSantini <danielsantini36@gmail.com>
2024-03-15 14:55:40 +0100

    ok

--- Controllers/OrderItemsController.cs
+++ Controllers/OrderItemsController.cs
@@ -8,6 +8,7 @@ using System.Web;
 using System.Web.Mvc;
 using AppPizzeria.Models;
 
+
 namespace AppPizzeria.Controllers
 {
     public class OrderItemsController : Controller
@@ -15,13 +16,58 @@ namespace AppPizzeria.Controllers
         private DBContext db = new DBContext();
 
         // GET: OrderItems
+        [Authorize]
         public ActionResult Index()
         {
             var orderItems = db.OrderItems.Include(o => o.OrderSummary).Include(o => o.Product);
             return View(orderItems.ToList());
         }
 
+   
+        [HttpPost]
+        [Authorize]
+        public ActionResult LinkCarrello(int id)
+        {
+            // Trova il prodotto desiderato
+            var product = db.Products.Find(id);
+            if (product == null)
+            {
+                // Gestisci il caso in cui il prodotto non viene trovato
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Controlla se l'utente ha già un carrello attivo
+            var userId = HttpContext.User.Identity.Name;
+            var cart = db.OrderSummaries.FirstOrDefault(o => o.UserId.ToString() == userId && o.State == "Non Evaso");
+            if (cart == null)
+            {
+                // se non c'è, crea un nuovo riepilogo d'ordine (carrello)
+                cart = new OrderSummary
+                {
+                    UserId = Convert.ToInt32(userId), // sostituisci con il metodo appropriato per ottenere l'id utente
+                    State = "non evaso"
+                };
+                db.OrderSummaries.Add(cart);
+                db.SaveChanges(); // salva per ottenere un ordersummaryid
+            }
+
+            // Aggiungi il nuovo OrderItem al carrello
+            OrderItem orderItem = new OrderItem
+            {
+                ProductId = product.ProductId,
+                OrderSummaryId = cart.OrderSummaryId,
+                ItemPrice = product.ProductPrice,
+                Quantity = 1 // Puoi impostare una quantità di default o passarla come parametro
+            };
+            db.OrderItems.Add(orderItem);
+            db.SaveChanges();
+
+            // Reindirizza l'utente alla vista del carrello
+            return RedirectToAction("Index");
+        }
+
         // GET: OrderItems/Details/5
+        [Authorize]
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -37,6 +83,7 @@ namespace AppPizzeria.Controllers
         }
 
         // GET: OrderItems/Create
+        [Authorize]
         public ActionResult Create()
         {
             ViewBag.OrderSummaryId = new SelectList(db.OrderSummaries, "OrderSummaryId", "OrderDate");
@@ -49,6 +96,7 @@ namespace AppPizzeria.Controllers
         // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult Create([Bind(Include = "OrderItemId,OrderSummaryId,ProductId,Quantity,ItemPrice")] OrderItem orderItem)
         {
             if (ModelState.IsValid)
@@ -64,6 +112,7 @@ namespace AppPizzeria.Controllers
         }
 
         // GET: OrderItems/Edit/5
+        [Authorize]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -85,20 +134,33 @@ namespace AppPizzeria.Controllers
         // Per altri dettagli, vedere https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "OrderItemId,OrderSummaryId,ProductId,Quantity,ItemPrice")] OrderItem orderItem)
+        [Authorize]
+        public ActionResult Edit(int OrderItemId, int Quantity)
         {
-            if (ModelState.IsValid)
+            var orderItem = db.OrderItems.Find(OrderItemId);
+            var product = db.Products.FirstOrDefault(o => o.ProductId == orderItem.ProductId);
+            if (orderItem == null)
             {
-                db.Entry(orderItem).State = EntityState.Modified;
+                return HttpNotFound();
+            }
+
+            if (Quantity > 0) // Aggiungi qui ulteriori controlli se necessario
+            {
+                orderItem.Quantity = Quantity;
+                orderItem.ItemPrice = (decimal)Quantity * product.ProductPrice;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
+
+            // Se arrivi qui, c'è stato un problema
             ViewBag.OrderSummaryId = new SelectList(db.OrderSummaries, "OrderSummaryId", "OrderDate", orderItem.OrderSummaryId);
             ViewBag.ProductId = new SelectList(db.Products, "ProductId", "ProductName", orderItem.ProductId);
             return View(orderItem);
         }
 
+
         // GET: OrderItems/Delete/5
+        [Authorize]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -116,6 +178,7 @@ namespace AppPizzeria.Controllers
         // POST: OrderItems/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize]
         public ActionResult DeleteConfirmed(int id)
         {
             OrderItem orderItem = db.OrderItems.Find(id);
--- Controllers/OrderSummariesController.cs
+++ Controllers/OrderSummariesController.cs
@@ -15,11 +15,108 @@ namespace AppPizzeria.Controllers
         private DBContext db = new DBContext();
 
         // GET: OrderSummaries
+        [Authorize]
         public ActionResult Index()
         {
-            var orderSummaries = db.OrderSummaries.Include(o => o.User);
-            return View(orderSummaries.ToList());
+            int userId = Convert.ToInt32(HttpContext.User.Identity.Name);
+            OrderSummary orderSummary = db.OrderSummaries
+                                          .Include(o => o.User)
+                                          .FirstOrDefault(o => o.UserId == userId && o.State == "Non Evaso");
+
+            if (orderSummary != null)
+            {
+                var orderItems = db.OrderItems.Include(o => o.Product).Where(oi => oi.OrderSummaryId == orderSummary.OrderSummaryId).ToList();
+                ViewBag.Somma = orderItems.Sum(oi => oi.ItemPrice);
+
+                return View(orderSummary);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
         }
+
+
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ConfermaOrdine(OrderSummary summary)
+        {
+            if (summary == null)
+            {
+                return View("Error"); // Gestire il caso di riepilogo ordine nullo
+            }
+
+            var userId = Convert.ToInt32(HttpContext.User.Identity.Name); // Assicurati che questo sia il modo corretto in cui recuperi l'ID utente
+
+            if (userId <= 0)
+            {
+                return View("Error", new { message = "Impossibile recuperare l'ID dell'utente valido." });
+            }
+
+            var existingOrderSummary = db.OrderSummaries.FirstOrDefault(o => o.UserId == userId && o.State == "Non Evaso");
+
+            if (existingOrderSummary != null)
+            {
+                decimal sommaPrezzo = db.OrderItems.Where(oi => oi.OrderSummaryId == existingOrderSummary.OrderSummaryId).Sum(o => o.ItemPrice);
+                
+                existingOrderSummary = new OrderSummary
+                {
+                    UserId = userId,
+                    State = "Evaso",
+                    OrderDate = summary.OrderDate,
+                    OrderAddress = summary.OrderAddress,
+                    Note = summary.Note,
+                    TotalPrice = sommaPrezzo,
+                };
+                db.OrderSummaries.Add(existingOrderSummary);
+                db.SaveChanges(); // Salva prima per ottenere un OrderSummaryId se necessario
+            }
+
+            // Reindirizza alla pagina Index di OrderSummaries
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult FinalizeOrder(int OrderSummaryId, string OrderAddress, string OrderDate, string Note)
+        {
+            if (OrderSummaryId == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest, "Identificativo ordine non valido");
+            }
+            OrderSummary orderSummary = db.OrderSummaries.Find(OrderSummaryId);
+            if (orderSummary == null)
+            {
+                return HttpNotFound("Ordine non trovato");
+            }
+
+            if (!string.IsNullOrEmpty(OrderAddress) && !string.IsNullOrEmpty(Note))
+            {
+                // Aggiorna i campi dell'ordine con i nuovi valori forniti dall'utente.
+                orderSummary.OrderAddress = OrderAddress;
+                orderSummary.OrderDate = OrderDate;
+                orderSummary.Note = Note;
+                orderSummary.State = "Evaso";
+
+                // Salva le modifiche nel database
+                db.Entry(orderSummary).State = EntityState.Modified;
+                db.SaveChanges();
+
+                // Reindirizza l'utente a una pagina di conferma o alla pagina di riepilogo degli ordini
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                // Gestisci il caso in cui l'indirizzo o le note siano vuoti
+                // Ritorna alla vista con un messaggio di errore
+                ModelState.AddModelError("", "Indirizzo di consegna e note sono obbligatori.");
+                return View(orderSummary); // Assicurati che questa vista esista o adatta secondo il tuo caso
+            }
+        }
+
+
 
         // GET: OrderSummaries/Details/5
         public ActionResult Details(int? id)
